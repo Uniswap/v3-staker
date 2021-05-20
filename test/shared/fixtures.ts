4b976f012d828af12e3ba6323b6ae9be1b890047
cf <sudocarlosflores@protonmail.com>
2021-05-20 12:20:38 -0700

    bug: fix bad import (#27)

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -12,7 +12,7 @@ import WETH9 from '../contracts/WETH9.json'
 import { linkLibraries } from './linkLibraries'
 import { INonfungiblePositionManager } from '../../typechain'
 import type { TestERC20 } from '../../typechain'
-import { UniswapV3Staker } from '../../typechain/UniswapV3Staker'
+import { UniswapV3Staker } from '../../typechain'
 import { FeeAmount, BigNumber, encodePriceSqrt } from '../shared'
 
 type IWETH9 = any
