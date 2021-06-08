458893f75dd9a0326e5b487696bbbdf6b0e4501b
Noah Zinsmeister <noahwz@gmail.com>
2021-06-08 12:19:14 -0400

    prettier

--- contracts/UniswapV3Staker.sol
+++ contracts/UniswapV3Staker.sol
@@ -226,7 +226,11 @@ contract UniswapV3Staker is IUniswapV3Staker, Multicall {
     }
 
     /// @inheritdoc IUniswapV3Staker
-    function withdrawToken(uint256 tokenId, address to, bytes memory data) external override {
+    function withdrawToken(
+        uint256 tokenId,
+        address to,
+        bytes memory data
+    ) external override {
         Deposit memory deposit = deposits[tokenId];
         require(
             deposit.numberOfStakes == 0,
@@ -240,7 +244,12 @@ contract UniswapV3Staker is IUniswapV3Staker, Multicall {
         delete deposits[tokenId];
         emit DepositTransferred(tokenId, deposit.owner, address(0));
 
-        nonfungiblePositionManager.safeTransferFrom(address(this), to, tokenId, data);
+        nonfungiblePositionManager.safeTransferFrom(
+            address(this),
+            to,
+            tokenId,
+            data
+        );
     }
 
     /// @inheritdoc IUniswapV3Staker
