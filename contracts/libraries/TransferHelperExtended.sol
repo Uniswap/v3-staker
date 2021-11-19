846e1ebf9d4c5fda376d1a31f3bae944e6a5a175
Emily Williams <emag3m@gmail.com>
2021-11-19 18:47:07 -0500

    remove unneeded functions

--- contracts/libraries/TransferHelperExtended.sol
+++ contracts/libraries/TransferHelperExtended.sol
@@ -36,25 +36,4 @@ library TransferHelperExtended {
         require(token.isContract(), 'TransferHelperExtended::safeTransfer: call to non-contract');
         TransferHelper.safeTransfer(token, to, value);
     }
-
-    /// @notice Approves the stipulated contract to spend the given allowance in the given token
-    /// @dev Errors with 'SA' if transfer fails
-    /// @param token The contract address of the token to be approved
-    /// @param to The target of the approval
-    /// @param value The amount of the given token the target will be allowed to spend
-    function safeApprove(
-        address token,
-        address to,
-        uint256 value
-    ) internal {
-        TransferHelper.safeApprove(token, to, value);
-    }
-
-    /// @notice Transfers ETH to the recipient address
-    /// @dev Fails with `STE`
-    /// @param to The destination of the transfer
-    /// @param value The value to be transferred
-    function safeTransferETH(address to, uint256 value) internal {
-        TransferHelper.safeTransferETH(to, value);
-    }
 }
